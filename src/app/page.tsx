a32759ab1e81542f8334af6ba4fb7f4a5fa72b0d
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-01 22:25:27 +0600

    fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   // inspiration for the design:
@@ -7,9 +7,6 @@ export default function Home() {
   return (
     <div className=" flex flex-col md:flex-row">
 
-
-      {/* <div className="h-80  md:w-1/2 bg-amber-200"></div>
-      <div className="h-80  md:w-1/2 bg-red-400"></div> */}
       <p className="">About me</p>
       <div className="text-7xl">Tanbir Hossen</div>
     </div>
