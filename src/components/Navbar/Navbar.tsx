1b537df28dd9bf2388318cf89cfc6affc9bea94d
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-15 13:30:13 +0600

    feat: added animated nav logo in navbar and added theme switcher

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,20 @@
+import Link from "next/link"
+import NavLogo from "./NavLogo"
+import { ThemeToggle } from "../ThemeToggle/ThemeToggle"
+import Container from "../Common/Container"
 
-export default function Navbar() {
+export default function Navbar({ className }: { className?: string }) {
   return (
-    <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
-      <div>Md Tanbir Hossen</div>
-
-      <div>
-        <ul className=" gap-4 hidden md:flex">
-            <li>Home</li>
-            <li>About me</li> 
-            <li>contact</li>
-            <li>Resume</li>
-        </ul>
-        
+    <Container className={`flex justify-between items-center px-4 py-2 text-[#1e2832] border-[#ececec] border-2 rounded-full my-4 sticky top-4  bg-opacity-80 backdrop-blur-xl z-50 ${className}`}>
+      <NavLogo />
+      <div className=" gap-4 hidden md:flex items-center dark:text-white">
+        <Link href={'/'}>Home</Link>
+        <Link href={'/project'}>Project</Link>
+        <Link href={'/about-me'}>About me</Link>
+        <Link target="_blank" href={'/contact'}>Contact</Link>
+        <a target="_blank" href="">Resume</a>
+        <ThemeToggle />
       </div>
-
-    </div>
+    </Container>
   )
 }
