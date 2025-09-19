0205c0dda9494298766d0cadd6ccaf7bb3fa2979
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-19 11:51:47 +0600

    light theme background fixed

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -3,10 +3,10 @@ import Footer from "@/components/Footer/Footer";
 import Loading from "@/components/Loading/Loading";
 import Navbar from "@/components/Navbar/Navbar";
 import { useEffect, useState } from "react";
-import { BackgroundBeams } from "../ui/background-beams";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     const [loading, setLoading] = useState(true)
+
     useEffect(() => {
         const timeOut = setTimeout(() => {
             setLoading(false)
@@ -17,13 +17,18 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
     if (loading) return <Loading />
 
     return (
-        <div className="px-2 bg-primary-light dark:bg-primary-dark text-primary-dark dark:text-primary-light pb-3">
-            <Navbar />
-            <main className="min-h-[calc(100vh-150px)] ">
-                {children}
-            </main>
-            <Footer />
-            <BackgroundBeams />
+        <div className="relative min-h-screen overflow-hidden">
+            {/* Moving Grid Background */}
+            <div className="absolute inset-0 -z-10 bg-animated-grid-light dark:bg-animated-grid-dark"></div>
+
+            {/* Main Content */}
+            <div className="px-2 bg-primary-light/70 dark:bg-primary-dark/70 text-primary-dark dark:text-primary-light pb-3 relative z-10 backdrop-blur-[2px]">
+                <Navbar />
+                <main className="min-h-[calc(100vh-145px)]">
+                    {children}
+                </main>
+                <Footer />
+            </div>
         </div>
     )
 }
