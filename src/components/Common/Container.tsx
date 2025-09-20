b6cd047a1cb4b0ff800ba2b94b9f1f4dcf500785
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-20 17:33:58 +0600

    updated container component

--- src/components/Common/Container.tsx
+++ src/components/Common/Container.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export default function Container({ children, className }: { children: ReactNode, className?: string }) {
     return (
-        <div className={`max-w-7xl mx-auto ${className}`}>
+        <div className={`max-w-7xl mx-auto px-2 ${className}`}>
             {children}
         </div>
     )
