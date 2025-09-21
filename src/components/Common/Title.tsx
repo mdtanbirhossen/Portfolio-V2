d5c571261614655076490dfc45f84bc90229ab2c
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-21 12:01:57 +0600

    created reuseable title

--- src/components/Common/Title.tsx
+++ src/components/Common/Title.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Title({ title, subtitle, className }: { title: string, subtitle?: string, className?: string }) {
     return (
-        <div className={`${className}`}>
-            <h3 className='text-primary-dark dark:text-primary-light'></h3>
-            <p className='text-secondary-dark dark:text-secondary-light-text'></p>
+        <div className={`flex flex-col justify-center text-center w-full space-y-2 md:space-y-4 mb-12 ${className}`}>
+            <h3 data-animation="text" className='text-primary-dark dark:text-primary-light text-2xl md:text-4xl lg:text-5xl'>{title}</h3>
+            <p data-animation="text" className='text-secondary-dark dark:text-secondary-light-text mx-auto w-full text-base sm:text-lg lg:max-w-3xl'>{subtitle}</p>
         </div>
     )
 }
