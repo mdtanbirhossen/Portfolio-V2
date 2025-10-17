bc98e1ba0c5fe24a5a54bee344fa7c7e5c807178
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-10-17 18:36:41 +0600

    added visa navigator and lingo bingo in projects page

--- src/components/FeaturedProjects/projects.tsx
+++ src/components/FeaturedProjects/projects.tsx
@@ -25,35 +25,32 @@ const projects = [
         server_repo_link: "https://github.com/mdtanbirhossen/Tour-Nest-Server"
     },
     {
-        name: "Tour Nest",
-        description: "Tour nest is an Tourist management web application. Here you can hire any of our tour guide and book different location's trips. And you can pay with stripe easily.",
-        featuredImage: "/assets/projects/tour-nest.png",
+        name: "Visa Navigator",
+        description: "Visa Navigator is an visa management web application. Here you can book many visas of world wide. And you can authenticate easily google and other gmail methods.",
+        featuredImage: "/assets/projects/visa-navigator.png",
         tags: [
             "React JS",
             "Javascript",
             "Tailwind CSS",
             "MongoDB",
             "Express",
-            "Stripe",
+            "Firebase"
         ],
-        link: "https://tour-nest-a8f37.web.app/",
-        client_repo_link: "https://github.com/mdtanbirhossen/Tour-Nest-Client",
-        server_repo_link: "https://github.com/mdtanbirhossen/Tour-Nest-Server"
+        link: "https://visa-navigator-client-beta.vercel.app/",
+        client_repo_link: "https://github.com/mdtanbirhossen/Visa-Navigator-Client",
+        server_repo_link: "https://github.com/mdtanbirhossen/Visa-Navigator-Server"
     },
     {
-        name: "Tour Nest",
-        description: "Tour nest is an Tourist management web application. Here you can hire any of our tour guide and book different location's trips. And you can pay with stripe easily.",
-        featuredImage: "/assets/projects/tour-nest.png",
+        name: "Lingo Bingo",
+        description: "A Language learning web Application. Here you can learn arabic language easily. And you can hear the word pronunciation.",
+        featuredImage: "/assets/projects/lingo-bingo.png",
         tags: [
             "React JS",
             "Javascript",
             "Tailwind CSS",
-            "MongoDB",
-            "Express",
         ],
-        link: "https://tour-nest-a8f37.web.app/",
-        client_repo_link: "https://github.com/mdtanbirhossen/Tour-Nest-Client",
-        server_repo_link: "https://github.com/mdtanbirhossen/Tour-Nest-Server"
+        link: "https://lingo-bingo-aecbd.web.app/",
+        client_repo_link: "https://github.com/mdtanbirhossen/Lingo-Bingo"
     },
     // {
     //     name: "FitStat – Personal Training & Progress Tracker",
