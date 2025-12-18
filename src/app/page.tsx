581e32252b4c5f8c5136537cb65455b02ea55d3b
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-12-18 19:08:36 +0600

    chore: upgrade Next.js to fix security vulnerability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,16 @@ import Banner from "@/components/Banner/Banner";
 import Experience from "@/components/Experience/Experience";
 import FeaturedProjects from "@/components/FeaturedProjects/FeaturedProjects";
 import GithubContribution from "@/components/GithubContribution/GithubContribution";
-// import Navbar from "@/components/Navbar/Navbar";
 import Skill from "@/components/Skill/Skill";
 
 export default async function Home() {
   return (
     <>
-
       <Banner />
       <Experience />
       <Skill />
       <GithubContribution />
       <FeaturedProjects />
-
     </>
   );
 }
